9e2c70ef45129f299a84e9db99cbe7af91ba2439
Robert Williams <robert@a3marketing.com>
2020-06-18 20:56:13 -0500

    added activate to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "app.js", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js",];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -14,3 +14,20 @@ self.addEventListener("install", function(evt) {
 
   self.skipWaiting();
 });
+
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
